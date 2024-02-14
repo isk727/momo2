102b3d17ed50b13cba892801f881ee9f67014cb8
isk727 <isk727@gmail.com>
2024-02-15 05:49:36 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const video = document.getElementById('remote_video');
 $("#sound").on('click', function () {
   if(video.muted){
-    $("#speaker").attr('src', 'img/sound_on.png');
+    $("#speaker").attr('src', 'img/sp_on.png');
     video.muted = false;
   }else{
-    $("#speaker").attr('src', 'img/sound_off.png');
+    $("#speaker").attr('src', 'img/sp_off.png');
     video.muted = true;
   }
 });
-window.setTimeout(function(){
+window.setTimeout(function() {
   connect();
 }, 1000);
-//connect();
